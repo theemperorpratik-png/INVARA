5c2edf1619079ff19cb5f10d7b373548622b25f2
theemperorpratik-png <theemperorpratik@gmail.com>
2026-08-16 13:58:11 +0545

    short commit tested clicks

---
+++ public/js/domain.js
@@ -0,0 +1,12 @@
+const cOpt = document.getElementById("cOpt")
+const eOpt = document.getElementById("eOpt")
+const aOpt = document.getElementById("aOpt")
+cOpt.addEventListener("click", ()=>{
+    console.log("CREATE AHH CLICKED")
+})
+eOpt.addEventListener("click", ()=>{
+    console.log("EXPLORE AHH CLICKED")
+})
+aOpt.addEventListener("click", ()=>{
+    console.log("ARCHIVE AHH CLICKED")
+})
