0dcfb11c1f1d0a6318850eac92cfe6a67994961a
theemperorpratik-png <theemperorpratik@gmail.com>
2026-08-20 09:02:54 +0545

    am not satisfied but still expirementing

--- public/js/create.js
+++ public/js/create.js
@@ -222,9 +222,46 @@ for (let i = 0; i < nebulaCount; i++) {
 
     }
 }
+//now comes the main fractal function
+function fractal() {
+    ctx.fillStyle = "red"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    ctx.strokeStyle = "white"
+    centerX = canvas.width/2
+    centerY = canvas.height/2
+    branch(
+    canvas.width / 2,
+    canvas.height,
+    150,
+    -Math.PI / 2,
+    Math.floor(Number(complexity.value)/10)
+)
+
+}
+function branch(x, y, length, angle, depth) {
+
+    if (depth <= 0) {
+        return
+    }
+
+    // calculate where this branch ends
+    const endX = x + Math.cos(angle) * length
+    const endY = y + Math.sin(angle) * length
+
+    // draw branch
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(endX, endY)
+    ctx.stroke()
+
+    // create smaller branches
+        branch(endX, endY, length * 0.7, angle - 0.5, depth - 1)
+        branch(endX, endY, length * 0.7, angle + 0.5, depth - 1)
+    
+}
 genButt.addEventListener("click", ()=>{
     
-    cosmos()
+    fractal()
 })
 rButt.addEventListener("click", ()=>{
     const newSeed = Math.floor(Math.random() * 999999)
