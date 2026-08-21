dbfa1f3701d13c4275b76530cc5ef75d951b6bfc
theemperorpratik-png <theemperorpratik@gmail.com>
2026-08-21 11:02:00 +0545

    Improved code and parameters for 1st fractal and prototype of second fractal is here

--- public/js/create.js
+++ public/js/create.js
@@ -24,13 +24,18 @@ function random(seed) {
     let x = Math.sin(seed++) * 10000
     return x - Math.floor(x)
 }
-
 sysSelect.addEventListener("change", (e) => {
     const selVal = e.target.value
     if (selVal === "cosmic") {
         console.log("cosmos selected")
     }
     else if (selVal === "fractal") {
+        document.getElementById("angle").addEventListener("input", (e) => {
+            document.getElementById("angleVal").textContent = e.target.value
+        })
+        document.getElementById("complex").addEventListener("input", (e) => {
+            document.getElementById("complexVal").textContent = e.target.value
+        })
         const fracType = document.getElementById("fracType")
         const background = document.getElementById("backName")
         document.getElementById("cosmoh").style.display = "none"
@@ -48,23 +53,70 @@ sysSelect.addEventListener("change", (e) => {
             ctx.strokeStyle = findCol()
             cX = canvas.width / 2
             cY = canvas.height / 2
-            drawTree(cX, canvas.height-50, 130, -Math.PI / 2, 10);
+            
+            if(document.getElementById("fracType").value=="fracTree") {
+                drawTree(cX, canvas.height - 50, 130, -Math.PI / 2, 10);
+            }else if(document.getElementById("fracType").value=="set") {
+                draw()
+            }
 
         }
-        function drawTree(x,y,length,angle,branchWidth) {
-            const complex = 11 - document.getElementById("complex").value/10
-            if (length<complex) {
+        {
+            //code from:https://dev.to/foqc/mandelbrot-set-in-js-480o cuz it was too complex to understand and make on my own in a reasonable time i did modify it accourding to my way 
+            const MAX_ITERATION = 80
+            function mandelbrot(c) {
+                let z = { x: 0, y: 0 }, n = 0, p, d;
+                do {
+                    p = {
+                        x: Math.pow(z.x, 2) - Math.pow(z.y, 2),
+                        y: 2 * z.x * z.y
+                    }
+                    z = {
+                        x: p.x + c.x,
+                        y: p.y + c.y
+                    }
+                    d = Math.sqrt(Math.pow(z.x, 2) + Math.pow(z.y, 2))
+                    n += 1
+                } while (d <= 2 && n < MAX_ITERATION)
+                return [n, d <= 2]
+            }
+            const HEIGHT = canvas.height
+            const WIDTH = canvas.width
+            const REAL_SET = { start: -2, end: 1 }
+            const IMAGINARY_SET = { start: -1, end: 1 }
+
+            const colors = new Array(16).fill(0).map((_, i) => i === 0 ? '#000' : `#${((1 << 24) * Math.random() | 0).toString(16)}`)
+
+            function draw() {
+                for (let i = 0; i < WIDTH; i++) {
+                    for (let j = 0; j < HEIGHT; j++) {
+                        complex = {
+                            x: REAL_SET.start + (i / WIDTH) * (REAL_SET.end - REAL_SET.start),
+                            y: IMAGINARY_SET.start + (j / HEIGHT) * (IMAGINARY_SET.end - IMAGINARY_SET.start)
+                        }
+
+                        const [m, isMandelbrotSet] = mandelbrot(complex)
+                        ctx.fillStyle = colors[isMandelbrotSet ? 0 : (m % colors.length - 1) + 1]
+                        ctx.fillRect(i, j, 1, 1)
+                    }
+                }
+            }
+        }
+        function drawTree(x, y, length, angle, branchWidth) {
+            const complex = 5.5 - document.getElementById("complex").value / 20
+            if (length < complex) {
                 return
             }
-            const angChange = Number(document.getElementById("angle").value)/100
+            const angChange = Number(document.getElementById("angle").value) / 100
+
             ctx.beginPath()
-            ctx.moveTo(x,y)
+            ctx.moveTo(x, y)
             const x2 = x + length * Math.cos(angle)
             const y2 = y + length * Math.sin(angle)
-            ctx.lineTo(x2,y2)
+            ctx.lineTo(x2, y2)
             ctx.stroke()
-            drawTree(x2, y2, length*0.75,angle+angChange,branchWidth*0.7)
-            drawTree(x2, y2, length*0.75,angle-angChange,branchWidth*0.7)
+            drawTree(x2, y2, length * 0.75, angle + angChange, branchWidth * 0.7)
+            drawTree(x2, y2, length * 0.75, angle - angChange, branchWidth * 0.7)
         }
         genButt.addEventListener("click", () => {
 
